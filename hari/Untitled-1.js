3c9ef5f71d7f9cd25b975e32113fb98bb5662c3e
anu <anu123@gmail.com>
2022-12-26 16:06:29 +0530

    my files

---
+++ hari/Untitled-1.js
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        ``
+    </head>
+    <body>
+        <div>
+            <center>
+                <form>
+                <input type ="text" placeholder="enter the customer name"id="id1"/><br>
+                <input type ="text" placeholder="enter phone number"id="id2"/><br>   
+                <input type ="text" placeholder="enter the email id"id="id3"/><br>
+                <input type ="text" placeholder="enter spectacle type" id="id4"/><br>
+                <input type ="text" placeholder="enter the frame band" id="id5"/><br>
+                <input type ="text" placeholder="enter the frame type" id="id6"/><br>
+                <input type ="text" placeholder="enter the frame colour" id="id7"/><br>
+                <input type ="text" placeholder="enter the glass category" id="id8"/><br>
+                <input type ="text" placeholder="enter payment type" id="id9"/><br>
+                <input type ="text" placeholder="enter the card No" id="id10"/><br>
+             </form>
+         </center>
+       </div>
+     <body>
+</html>
+    
---
+++ hari/hh.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <body>
+            <div>
+                <center>
+                    <form>
+                        <input type="text"place holder="enter the customer name" id="id1"/><br>
+                        <input type="text"place holder="enter phone number" id="id2"/><br>
+                        <input type="text"place holder="enter the email id" id="id3"/><br>
+                        <input type="text"place holder="enter spectacle type" id="id4"/><br>
+                        <input type="text"place holder="enter " id="id"/><br>
+                        
+                </center>
+            </div>
+        </body>
+    </head>
+</html>
